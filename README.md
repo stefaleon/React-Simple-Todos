8e96cb26345ea58d745a216f1af2400ef810cef9
stefaleon <stefaleon@gmail.com>
2018-02-21 15:55:20 +0200

    Add app parts

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ### 00 Initial
 
 * Create app with *create-react-app*.
+
+
+### 01 Add app parts
+
+* App constructor
+* Form with an input and a button
+* Todo content div
+* TodoItem
--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const TodoItem = ({text}) => (
+  <li>{text}</li>
+);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+  }
   render() {
+    const { newTodo } = this.state;
+    const todos = this.state.todos.map((t, i) => (
+      <TodoItem key={i} text={t} />
+    ));
     return (
       <div className="App">
-
+        <h1>Simple Todo App</h1>
+        <form>
+          <input
+            className="todo-input"
+            autoComplete="off"
+            type="text"
+            name="newTodo"
+            placeHolder="What needs to be done?"
+            value={newTodo}
+          />
+          <button
+            type="submit"
+            className="save-button"
+          >
+            Save
+          </button>
+        </form>
+        <div className="todo-content">
+          <ol>
+            {todos}
+          </ol>
+        </div>
       </div>
     );
   }
